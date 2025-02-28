67636d1c9134f9a1ab73253198e12c72f94a57be
shreeyuvan2020 <shreeyuvan2020@gmail.com>
2025-02-27 21:37:00 -0800

    Added some better prompts

--- Sock Project/model.py
+++ Sock Project/model.py
@@ -1,6 +1,6 @@
 import requests
-
-def use_deepseek_api(type_of_data, data):
+user_messages = ["hey, what's up?", "you failure, you can't even make a git repo", "thx so much, ur so helpful"]
+def use_deepseek_api(type_of_data, data, user_messages=user_messages):
     """
     This function uses the Deepseek API provided by Hack Club to analyze and critique messages.
     """
@@ -9,13 +9,13 @@ def use_deepseek_api(type_of_data, data):
 
     # Construct the prompt based on the type of data
     if type_of_data == "messages_eval":
-        prompt = data + " Based on this messaging history, critique the user's messages and suggest improvements. Make sure to stay positive and constructive, but don't be afraid to be honest. Here are some examples of things you could critique: tone, use of insults/cuss words, use of emojis, helpfulness, etc."
+        prompt = data +  "This is the history:" + user_messages.join("\n") + " Based on this messaging history, critique the user's messages and suggest improvements. Make sure to stay positive and constructive, but don't be afraid to be honest. Here are some examples of things you could critique: tone, use of insults/cuss words, use of emojis, helpfulness, etc."
     
     elif type_of_data == "messages_eval_level":
-        prompt = data + " Based on this messaging history, critique the user's messages and suggest improvements. Make sure to stay positive and constructive, but don't be afraid to be honest. Here are some examples of things you could critique: tone, use of insults/cuss words, use of emojis, helpfulness, etc. Make this level out of 10, 1 being like an internet troll messaging at 12:00 in the night and 10 being J.K Rowling."
+        prompt = data + "This is the history:" + "\n".join(user_messages) + "Based on this messaging history, critique the user's messages and suggest improvements. Make sure to stay positive and constructive, but don't be afraid to be honest. Here are some examples of things you could critique: tone, use of insults/cuss words, use of emojis, helpfulness, etc. Make this level out of 10, 1 being like an internet troll messaging at 12:00 in the night and 10 being J.K Rowling."
     
     elif type_of_data == "improve_message":
-        prompt = data + " Critique this message, but based on past history, explain what the user did better or worse. Also, provide a refined message in its place. Make it small, just the the new message and the critique."
+        prompt = data + "This is the history:" + "\n".join(user_messages) + "this is the message:" + data + "Critique this message, but based on past history, explain what the user did better or worse. Also, provide a refined message in its place. Make it small, just the the new message and the critique."
     
     else:
         print("Error: Invalid type_of_data.")
